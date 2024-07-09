a52d2a4e0680d3a5092c3a957fb8bd677e532ae0
Thiago <Thiago@DESKTOP-HLR6O3L>
2024-07-09 09:47:49 -0300

    Adicionar arquivos de projeto.

---
+++ TEMA1/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TEMA1
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
